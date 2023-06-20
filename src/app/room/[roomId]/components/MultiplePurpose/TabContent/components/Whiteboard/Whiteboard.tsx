069d7fec095bb86f0f131e6a24973738936117ab
Aubis Sanchez <aubisst@gmail.com>
2023-06-20 01:04:44 -0400

    feat: implemented white board tab , with options

--- src/app/room/[roomId]/components/MultiplePurpose/TabContent/components/Whiteboard/Whiteboard.tsx
+++ src/app/room/[roomId]/components/MultiplePurpose/TabContent/components/Whiteboard/Whiteboard.tsx
@@ -1,4 +1,86 @@
+import {ReactPainter} from 'react-painter'
+import {LineCapType, LineJoinType} from 'react-painter/dist/ReactPainter'
+import ResetIcon from './components/ResetIcon'
+import {useState} from 'react'
+
 const Whiteboard = () => {
-  return <div>Whiteboard</div>
+  const [canvasRef, setCanvasRef] = useState<HTMLCanvasElement | null>(null)
+
+  return (
+    <div className="w-full h-full ">
+      <ReactPainter
+        width={500}
+        height={300}
+        onSave={blob => console.log(blob)}
+        render={({canvas, setColor, setLineWidth, setLineJoin, setLineCap}) => (
+          <div className="w-full h-full flex flex-row justify-evenly ">
+            <div className="flex flex-col h-full justify-center py-4 items-center gap-y-4 -mt-4">
+              <div className="flex flex-row items-center">
+                <label className="label">
+                  <span className="label-text">Line Width</span>
+                </label>
+                <input
+                  type="number"
+                  onChange={e => setLineWidth(Number(e.target.value))}
+                  min={1}
+                  max={6}
+                  defaultValue={3}
+                  className="input input-primary input-bordered input-sm w-auto "
+                />
+              </div>
+
+              <div className="flex flex-row items-center">
+                <label className="label">
+                  <span className="label-text">Color</span>
+                </label>
+                <input
+                  type="color"
+                  onChange={e => setColor(e.target.value)}
+                  className="input input-bordered input-sm w-12  outline-none border-none bg-transparent "
+                />
+              </div>
+
+              <div className="flex flex-row items-center">
+                <label className="label">
+                  <span className="label-text">Line cap</span>
+                </label>
+                <select
+                  className="select select-secondary w-auto select-sm"
+                  onChange={e => setLineCap(e.target.value as LineCapType)}
+                >
+                  <option value="round">round</option>
+                  <option value="butt">butt</option>
+                  <option value="square">square</option>
+                </select>
+              </div>
+              <div className="flex flex-row items-center">
+                <label className="label">
+                  <span className="label-text">Line Join</span>
+                </label>
+                <select
+                  className="select select-secondary w-auto select-sm"
+                  onChange={e => setLineJoin(e.target.value as LineJoinType)}
+                >
+                  <option value="round">round</option>
+                  <option value="bevel">bevel</option>
+                  <option value="miter">miter</option>
+                </select>
+              </div>
+              <button
+                // onClick={resetCanvas}
+                className="flex flex-row items-center btn btn-sm text-white"
+              >
+                Reset <ResetIcon />
+              </button>
+            </div>
+
+            <div id="canvas" className="bg-[#eee] overflow-hidden ">
+              {canvas}
+            </div>
+          </div>
+        )}
+      />
+    </div>
+  )
 }
 export default Whiteboard
---
+++ src/app/room/[roomId]/components/MultiplePurpose/TabContent/components/Whiteboard/components/ResetIcon.tsx
@@ -0,0 +1,24 @@
+const ResetIcon = () => {
+  return (
+    <div>
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="20"
+        height="20"
+        viewBox="0 0 24 24"
+        fill="none"
+        stroke="currentColor"
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        className="ai ai-ArrowCycle ml-2"
+      >
+        <path d="M22 12c0 6-4.39 10-9.806 10C7.792 22 4.24 19.665 3 16" />
+        <path d="M2 12C2 6 6.39 2 11.806 2 16.209 2 19.76 4.335 21 8" />
+        <path d="M7 17l-4-1-1 4" />
+        <path d="M17 7l4 1 1-4" />
+      </svg>
+    </div>
+  )
+}
+export default ResetIcon
