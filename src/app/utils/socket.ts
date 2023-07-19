396c4a3570dd0642991cdb4356c4e3a97edc5173
Aubis Sanchez <aubisst@gmail.com>
2023-07-19 14:04:06 -0400

    local server for socket

--- src/app/utils/socket.ts
+++ src/app/utils/socket.ts
@@ -2,5 +2,5 @@ import {Socket, io} from 'socket.io-client'
 import {DefaultEventsMap} from 'socket.io/dist/typed-events'
 
 export const socket: Socket<DefaultEventsMap, DefaultEventsMap> = io(
-  'https://programming-rooms-server-pahbjiz2k-aubisdev.vercel.app',
+  'http://localhost:4000',
 )
